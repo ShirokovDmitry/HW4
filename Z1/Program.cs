303528d6e67b869795d0af75a81806a47df8b21f
«Rawed» <pepe1@mail.ru>
2022-09-01 14:40:36 +0300

    save1

---
+++ Z1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число 1");
+double a =Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число 2");
+double b =Convert.ToDouble(Console.ReadLine());
+
+double sum = 0;
+sum = Math.Pow(a, b);
+Console.WriteLine("Сумма = " + sum);
+
+
+
---
+++ Z3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+Random rand = new Random();
+for (int i = 0; i < 5; i++)
+{
+    array[i] = rand.Next(0, 99);
+    Console.Write("{0}, ", array[i]);
+}
+Console.WriteLine();
+
+for (int i = 0; i < 3; i++)
+{
+    array[i] = rand.Next(0, 99);
+    Console.Write("{0}, ", array[i]);
+}
